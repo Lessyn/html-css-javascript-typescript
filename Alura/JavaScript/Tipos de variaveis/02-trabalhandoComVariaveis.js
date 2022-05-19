4caa8eb875e648ec18e44c922a1953811e268062
Rodrigo Lessyn de Paula <rodrigolessyn@gmail.com>
2022-05-19 11:11:53 -0300

    02.Tipos de variaveis

---
+++ Alura/JavaScript/Tipos de variaveis/02-trabalhandoComVariaveis.js
@@ -0,0 +1,10 @@
+//Console.log("Trabalhando com variáveis");
+//JS é case sensitive
+
+console.log("Trabalhando com variáveis");
+
+const idade = 29;
+const nome = "Ricardo";
+
+//mes = "Janeiro"
+//Não fazer
---
+++ Alura/JavaScript/Tipos de variaveis/05-atribuicaoVariaveis.js
@@ -0,0 +1,12 @@
+console.log("Atribuição de variáveis");
+
+const idade = 42;
+const primeiroNome = "Rodrigo";
+const sobrenome = "Lessyn";
+
+console.log(primeiroNome + " " + sobrenome);
+console.log(primeiroNome, sobrenome);
+console.log(`Meu nome é ${primeiroNome} ${sobrenome}`);
+
+
+
