7f5312b92f3f2e6d4c34853345552174b52d709e
Rodrigo Lessyn de Paula <rodrigolessyn@gmail.com>
2022-05-25 17:18:22 -0300

    04.Eventos, Formularios e Criando Elementos

---
+++ Alura/JavaScript/Programando na linguagem da web/04 - Eventos, Formulários e criando elementos/js/principal.js
@@ -0,0 +1,87 @@
+var titulo = document.querySelector(".titulo");
+titulo.textContent = "Aparecida Nutricionista";
+
+var pacientes = document.querySelectorAll(".paciente");
+
+for (var i = 0; i < pacientes.length; i++) {
+    var paciente = pacientes[i];
+
+    var tdPeso = paciente.querySelector(".info-peso");
+    var peso = tdPeso.textContent;
+
+    var tdAltura = paciente.querySelector(".info-altura");
+    var altura = tdAltura.textContent;
+
+    var tdImc = paciente.querySelector(".info-imc")
+
+    var alturaEhValida = true;
+    var pesoEhValido = true;
+
+    if (peso <= 0 || peso >= 1000) {
+        console.log("Peso inválido");
+        pesoEhValido = false;
+        tdImc.textContent = "Peso inválido";
+        paciente.classList.add("paciente-invalido");
+
+    }
+
+    if (altura <= 0 || altura >= 3.0) {
+        console.log("Altura inválida");
+        alturaEhValida = false;
+        tdImc.textContent = "Altura inválida";
+        paciente.classList.add("paciente-invalido");
+    }
+
+    if (pesoEhValido && alturaEhValida) {
+        var imc = (peso / (altura * altura));
+        tdImc.textContent = imc.toFixed(2);
+    }
+}
+
+/*titulo.addEventListener("click", function(){
+    console.log("OLha só, posso chamar uma função anônima.");
+});
+
+function mostraMensagem(){
+    console.log("Olá, eu fui clicado!");
+}*/ //Tipos de funções na quais o javascript possa "escutar" o movimento de click do usuário. Ae temos duas opções, uma é utilizando a função anônima ou passando a função "mostraMensagem" como parâmetros em .addEventListenner.
+
+var botaoAdicionar = document.querySelector("#adicionar-paciente");
+
+botaoAdicionar.addEventListener("click", function (event) {
+    event.preventDefault();
+    var form = document.querySelector("#form-adiciona");
+
+    var nome = form.nome.value;
+    var peso = form.peso.value;
+    var altura = form.altura.value;
+    var gordura = form.gordura.value;
+    //imc = form.imc.value;
+
+    //console.log(nome);
+    //console.log(peso);
+    //console.log(altura);
+    //console.log(gordura);
+
+    var pacienteTr = document.createElement("tr");
+
+    var nomeTd = document.createElement("td");
+    var pesoTd = document.createElement("td");
+    var alturaTd = document.createElement("td");
+    var gorduraTd = document.createElement("td");
+    var imcTd = document.createElement("td");
+
+    nomeTd.textContent = nome;
+    pesoTd.textContent = peso;
+    alturaTd.textContent = altura;
+    gorduraTd.textContent = gordura;
+
+    pacienteTr.appendChild(nomeTd);
+    pacienteTr.appendChild(pesoTd);
+    pacienteTr.appendChild(alturaTd);
+    pacienteTr.appendChild(gorduraTd);
+
+    var tabela = document.querySelector("#tabela-pacientes");
+
+    tabela.appendChild(pacienteTr);
+})
