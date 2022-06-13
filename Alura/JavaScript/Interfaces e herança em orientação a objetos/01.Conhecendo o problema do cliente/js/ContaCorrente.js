5f71300acadce6c1bae4aad608b09015565a1c37
Rodrigo Lessyn de Paula <rodrigolessyn@gmail.com>
2022-06-13 17:30:44 -0300

    02.02 - Heranca

--- Alura/JavaScript/Interfaces e herança em orientação a objetos/01.Conhecendo o problema do cliente/js/ContaCorrente.js
+++ Alura/JavaScript/Interfaces e herança em orientação a objetos/01.Conhecendo o problema do cliente/js/ContaCorrente.js
@@ -1,54 +1,11 @@
 import { Cliente } from "./Cliente.js";
+import { Conta } from "./Conta.js";
 
-export class ContaCorrente {
+export class ContaCorrente extends Conta {
 
-   static numeroDeContas = 0;   
-   
-    get saldo(){
-        return this._saldo;
-    }
-
-    set cliente(novoValor){
-        if(novoValor instanceof Cliente){
-            this._cliente = novoValor;
-        }        
-    }
-
-    get cliente(){
-        return this._cliente;
-    }
-
-    constructor(cliente, agencia){
-        this._agencia = agencia;
-        this._cliente = cliente;
-        this._saldo = 0;
+   static numeroDeContas = 0; 
+    constructor(agencia, cliente){ 
+        super( agencia, cliente, 0 );       
         ContaCorrente.numeroDeContas += 1;
-    }
-
-    sacar(valor) {
-        if (this._saldo >= valor) {
-            this._saldo -= valor;
-            console.log("O saque de R$" + valor + ", foi realizado com sucesso.");
-            return valor;
-        }
-        else {
-            console.log("Saldo indisponível para saque.");
-        }
-    }
-    depositar(valor) {
-        if (valor <= 0) {
-
-            console.log("Por favor, deposite um valor válido!");
-        }
-        else {            
-            this._saldo += valor;
-            console.log("Deposito realizado com sucesso! Seu novo saldo é de: R$" + this._saldo);
-        }
-
-    }
-    transferir(valor, conta) {
-        const valorSacado = this.sacar(valor);
-        conta.depositar(valorSacado);
-        console.log("Transferência de "+valorSacado+" é realizado com sucesso");
-    }
+    }  
 }
