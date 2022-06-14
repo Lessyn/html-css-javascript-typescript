eea27e6cf9617e814231856ac3d5a60719df0bab
Rodrigo Lessyn de Paula <rodrigolessyn@gmail.com>
2022-06-14 14:37:35 -0300

    03.Classes abstratas

---
+++ Alura/JavaScript/Interfaces e herança em orientação a objetos/03.Classes abstratas/js/index.js
@@ -0,0 +1,16 @@
+import { Cliente } from "./Cliente.js";
+import { ContaCorrente } from "./ContaCorrente.js";
+import { ContaPoupanca } from "./ContaPoupanca.js"; 
+import { Conta } from "./Conta.js";
+import { ContaSalario } from "./ContaSalario.js";
+
+const cliente1 = new Cliente("Ricardo", 11122233309);
+
+const contaCorrenteRicardo = new ContaCorrente(1001, cliente1);
+const contaPoupanca = new ContaPoupanca(1001, cliente1, 50);
+//const conta = new Conta(1001, cliente1, 50); Chamando incorretamente um método abstrato.
+const contaSalario = new ContaSalario(cliente1);
+contaSalario.depositar(100);
+contaSalario.sacar(10);
+
+console.log(contaSalario);
