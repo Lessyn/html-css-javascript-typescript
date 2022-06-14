51882842ea50a639242283f550e2b4ff97fa6fb3
Rodrigo Lessyn de Paula <rodrigolessyn@gmail.com>
2022-06-14 08:42:53 -0300

    Revert "Merge branch 'main' of https://github.com/Lessyn/html-css-javascript-typescript"
    This reverts commit cb34c6c1c8da1897efc5eb1155a1a4b2ac884bf1, reversing
    changes made to cf7f472b1217eda200106a3c25e8dfee1441f49b.

--- Alura/JavaScript/Interfaces e herança em orientação a objetos/01.Conhecendo o problema do cliente/js/Conta.js
+++ Alura/JavaScript/Interfaces e herança em orientação a objetos/01.Conhecendo o problema do cliente/js/Conta.js
@@ -1,46 +1,25 @@
-export class Conta {
-    constructor(agencia, cliente, saldoInicial) {
+export class Conta{
+    constructor(agencia, cliente, saldoInicial){
         this._agencia = agencia;
         this._cliente = cliente;
-        this._saldo = saldoInicial;
-    }
-
-    set cliente(novoValor) {
-        if (novoValor instanceof Cliente) {
-            this._cliente = novoValor;
-        }
-    }
-    get saldo() {
-        return this._saldo;
-    }
-
-    get cliente() {
-        return this._cliente;
+        this._saldo = saldoInicial;        
     }
     sacar(valor) {
-        let taxa = 1;
-        return this._sacar(valor, taxa);
-    }
-
-    _sacar(valor, taxa) {
-        const valorSacado = taxa * valor;
-        if (this._saldo >= valorSacado) {
-            this._saldo -= valorSacado;
-            console.log("O saque de R$" + valorSacado + ", foi realizado com sucesso.");
-            return valorSacado;
+        if (this._saldo >= valor) {
+            this._saldo -= valor;
+            console.log("O saque de R$" + valor + ", foi realizado com sucesso.");
+            return valor;
         }
         else {
             console.log("Saldo indisponível para saque.");
-            return 0;
         }
     }
-
     depositar(valor) {
         if (valor <= 0) {
 
             console.log("Por favor, deposite um valor válido!");
         }
-        else {
+        else {            
             this._saldo += valor;
             console.log("Deposito realizado com sucesso! Seu novo saldo é de: R$" + this._saldo);
         }
@@ -48,6 +27,6 @@ export class Conta {
     transferir(valor, conta) {
         const valorSacado = this.sacar(valor);
         conta.depositar(valorSacado);
-        console.log("Transferência de " + valorSacado + " é realizado com sucesso");
+        console.log("Transferência de "+valorSacado+" é realizado com sucesso");
     }
 }
--- Alura/JavaScript/Interfaces e herança em orientação a objetos/01.Conhecendo o problema do cliente/js/index.js
+++ Alura/JavaScript/Interfaces e herança em orientação a objetos/01.Conhecendo o problema do cliente/js/index.js
@@ -3,17 +3,18 @@ import { ContaCorrente } from "./ContaCorrente.js";
 import { ContaPoupanca } from "./ContaPoupanca.js"; 
 import { Conta } from "./Conta.js";
 
+
 const cliente1 = new Cliente("Ricardo", 11122233309);
 
-const contaCorrenteRicardo = new ContaCorrente(1001, cliente1);
+const contaCorrenteRicardo = new Conta(0, cliente1, 1001);
+console.log(cliente1.nome +", seu saldo é: "+contaCorrenteRicardo._saldo)   
+
 contaCorrenteRicardo.depositar(500);
 contaCorrenteRicardo.sacar(100);
-console.log(cliente1.nome +", seu saldo é: "+contaCorrenteRicardo._saldo)    
-
 console.log(cliente1.nome +", seu novo saldo é de: R$" +contaCorrenteRicardo._saldo);
 
 console.log("A quantidade de contas correntes são: "+Conta.numeroDeContas);
-const contaPoupanca = new ContaPoupanca(1001, cliente1, 50);
+const contaPoupanca = new Conta(1001, cliente1, 50);
 
 console.log(contaPoupanca);
 console.log(contaCorrenteRicardo);
