f833c53ee255c8533c06d03e021d920186e9d524
Rodrigo Lessyn de Paula <rodrigolessyn@gmail.com>
2022-06-14 17:12:51 -0300

    03.Classes abstratas

--- Alura/JavaScript/Interfaces e herança em orientação a objetos/03.Classes abstratas/js/index.js
+++ Alura/JavaScript/Interfaces e herança em orientação a objetos/03.Classes abstratas/js/index.js
@@ -1,7 +1,6 @@
 import { Cliente } from "./Cliente.js";
 import { ContaCorrente } from "./ContaCorrente.js";
-import { ContaPoupanca } from "./ContaPoupanca.js"; 
-import { Conta } from "./Conta.js";
+import { ContaPoupanca } from "./ContaPoupanca.js";
 import { ContaSalario } from "./ContaSalario.js";
 
 const cliente1 = new Cliente("Ricardo", 11122233309);
