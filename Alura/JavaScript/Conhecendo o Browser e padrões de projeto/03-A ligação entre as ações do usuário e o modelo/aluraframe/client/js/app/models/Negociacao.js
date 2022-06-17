0f86e5d480da787119980b9d3ae596ce87a3cf79
Rodrigo Lessyn de Paula <rodrigolessyn@gmail.com>
2022-06-17 17:24:26 -0300

    03.A ligacao entre as acoes do usuario e o modelo

---
+++ Alura/JavaScript/Conhecendo o Browser e padrões de projeto/03-A ligação entre as ações do usuário e o modelo/aluraframe/client/js/app/models/Negociacao.js
@@ -0,0 +1,22 @@
+class Negociacao{
+    constructor(data, quantidade, valor){
+        this._data  = new Date( data.getTime());
+        this._quantidade = quantidade;
+        this._valor = valor;
+        Object.freeze(this);
+    }//Explicação com relação a método e função. Se é criada uma "função" dentro de uma classe, a gente a chama de método. Mas se essa "função" é criada fora da classe, ela continuará sendo chamada de função.
+    get obtemVolume(){
+
+        return this._quantidade * this._valor;
+    }
+    get data(){
+        return new Date( this._data.getTime());
+    }
+    get quantidade(){
+        return this._quantidade;
+    }
+    get valor(){
+        return this._valor;
+    }
+
+}
