1f67517e528ed33ba6439b9e3e1cc1781cb80bec
Rodrigo Lessyn de Paula <rodrigolessyn@gmail.com>
2022-06-21 15:32:42 -0300

    04.06.Blindando as negociacoes dentro da lista

--- Alura/JavaScript/Conhecendo o Browser e padrões de projeto/04-Lidar com data é trabalhoso Chame um ajudante/aluraframe/client/js/app/controllers/NegociacaoController.js
+++ Alura/JavaScript/Conhecendo o Browser e padrões de projeto/04-Lidar com data é trabalhoso Chame um ajudante/aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -10,19 +10,20 @@ class NegociacaoController {
     }
 
     adiciona(event) {
+
         event.preventDefault();
-        
-       this._listaNegociacoes.adiciona(this._criaNegociacao());
-       this._limpaFormulario();
-       console.log(this._listaNegociacoes.negociacoes);
+        this._listaNegociacoes.adiciona(this._criaNegociacao());
+        this._limpaFormulario();
     }
     _criaNegociacao() {
+
         return new Negociacao(
             DateHelper.textoParaData(this._inputData.value),
             this._inputQuantidade.value,
             this._inputValor.value);
     }
     _limpaFormulario() {
+
         this._inputData.value = '';
         this._inputQuantidade.value = 1;
         this._inputValor.value = 0.0;
--- Alura/JavaScript/Conhecendo o Browser e padrões de projeto/04-Lidar com data é trabalhoso Chame um ajudante/aluraframe/client/js/app/models/ListaNegociacoes.js
+++ Alura/JavaScript/Conhecendo o Browser e padrões de projeto/04-Lidar com data é trabalhoso Chame um ajudante/aluraframe/client/js/app/models/ListaNegociacoes.js
@@ -1,12 +1,12 @@
 class ListaNegociacoes {
     constructor(){
-        this._negociacoes = []
+        this._negociacoes = [];
     }
     adiciona(negociacao) {
         this._negociacoes.push(negociacao);
 
     }
     get negociacoes () {
-        return this._negociacoes; 
+        return [].concat(this._negociacoes); 
     }
 }
