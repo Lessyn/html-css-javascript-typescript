b9fb01e42975715c903d2c57308c6fadebab8c52
Rodrigo Lessyn de Paula <rodrigolessyn@gmail.com>
2022-06-21 10:44:45 -0300

    04.03. Metodos estaticos

--- Alura/JavaScript/Conhecendo o Browser e padrões de projeto/04-Lidar com data é trabalhoso Chame um ajudante/aluraframe/client/js/app/helpers/DateHelper.js
+++ Alura/JavaScript/Conhecendo o Browser e padrões de projeto/04-Lidar com data é trabalhoso Chame um ajudante/aluraframe/client/js/app/helpers/DateHelper.js
@@ -1,10 +1,13 @@
 class DateHelper {
-    dataParaTexto(data) {
+constructor(){
+    throw new Error("Essa classe não pode ser instânciada"); 
+}
+     static dataParaTexto(data) {
         return data.getDate()
             + '/' + (data.getMonth() + 1)
             + '/' + data.getFullYear();
     }
-    textoParaData(texto) {
+     static textoParaData(texto) {
         return new Date(...texto.split('-').map((item, indice) => item - indice % 2));
     }
 }
