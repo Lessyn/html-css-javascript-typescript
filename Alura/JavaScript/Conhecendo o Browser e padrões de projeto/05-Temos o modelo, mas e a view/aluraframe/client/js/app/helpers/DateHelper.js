b6c2a37391643ec82a9811176b68ec9a52631375
Rodrigo Lessyn de Paula <rodrigolessyn@gmail.com>
2022-06-22 08:52:08 -0300

    Importando arquivos do modulo 04.

---
+++ Alura/JavaScript/Conhecendo o Browser e padrões de projeto/05-Temos o modelo, mas e a view/aluraframe/client/js/app/helpers/DateHelper.js
@@ -0,0 +1,18 @@
+class DateHelper {
+    constructor() {
+
+        throw new Error("Essa classe não pode ser instânciada");
+    }
+    static dataParaTexto(data) {
+
+        return `${data.getDate()}/${data.getMonth() + 1}/${get.getFullYear()}`;
+    }
+
+    static textoParaData(texto) {
+        
+        if (!/\d{4}-\d{2}-\d{2}/.test(texto)) {
+            throw new Error("Deve estar no formato aaaa-mm-dd");
+        }
+        return new Date(...texto.split('-').map((item, indice) => item - indice % 2));
+    }
+}
