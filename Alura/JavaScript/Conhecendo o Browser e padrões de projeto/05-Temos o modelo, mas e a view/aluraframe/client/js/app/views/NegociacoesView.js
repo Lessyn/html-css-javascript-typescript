249210fa996a6fb56dec4c3553703f2078cb5f5c
Rodrigo Lessyn de Paula <rodrigolessyn@gmail.com>
2022-06-22 09:57:17 -0300

    05.04.Totalizando o volume em nosso template

--- Alura/JavaScript/Conhecendo o Browser e padrões de projeto/05-Temos o modelo, mas e a view/aluraframe/client/js/app/views/NegociacoesView.js
+++ Alura/JavaScript/Conhecendo o Browser e padrões de projeto/05-Temos o modelo, mas e a view/aluraframe/client/js/app/views/NegociacoesView.js
@@ -27,11 +27,21 @@ class NegociacoesView {
                 <td>${n.volume}</td>
               </tr>
             `
-            
+
         ).join('')}
             </tbody>
 
             <tfoot>
+            <td colspan="3"></td>
+            <td>
+            ${
+                (function(){
+                    let total = 0;
+                    model.negociacoes.forEach(n => total+= n.volume);
+                    return total;                        
+                    })()
+            }
+            </td>
             </tfoot>
         </table>
         `;
