1e53a5aae9d81f2b9eada9315939add97f50ee30
Rodrigo Lessyn de Paula <rodrigolessyn@gmail.com>
2022-06-28 08:01:59 -0300

    04.02.Requisicoes Ajax com o objeto XMLHttpRequest

--- Alura/JavaScript/Aprofundando em MVC, padrão Proxy e Factory/04-Importando negociações/aluraframe/client/js/app/controllers/NegociacaoController.js
+++ Alura/JavaScript/Aprofundando em MVC, padrão Proxy e Factory/04-Importando negociações/aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -11,7 +11,7 @@ class NegociacaoController {
         this._listaNegociacoes = new Bind(
             new ListaNegociacoes(),
             new NegociacoesView($('#negociacoesView')),
-            'adiciona', 'esvazia');        
+            'adiciona', 'esvazia');
 
         this._mensagem = new Bind(
             new Mensagem(),
@@ -28,6 +28,33 @@ class NegociacaoController {
         this._mensagem.texto = 'Negociação adicionada com sucesso!!'
 
         this._limpaFormulario();
+    }
+
+    importaNegociacoes() {
+        let xhr = new XMLHttpRequest();
+
+        xhr.open('GET', 'negociacoes/semana');
+
+        xhr.onreadystatechange = () => {
+            /*Estados de uma requisição AJAX:
+            0: requisição ainda não iniciada
+            1: conexão com o servidor estabelecida
+            2: requisição recebida
+            3: processando requisição
+            4: requisição concluída e a resposta esta pronta*/
+
+            if(xhr.readyState == 4){
+                if(xhr.status == 200){
+
+                    console.log("Obtendo as negociações do servidor.");
+                }
+                else{
+                    console.log("Nâo foi possível obter as negociações do servidor.");
+                }
+            }
+        };
+
+        xhr.send()
     }
 
     apagar() {
