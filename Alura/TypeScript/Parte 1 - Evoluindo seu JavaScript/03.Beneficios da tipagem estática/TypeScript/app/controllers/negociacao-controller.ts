36e1a749a1e2d655dde34966c4e0656e9e941f95
Rodrigo Lessyn de Paula <rodrigolessyn@gmail.com>
2022-06-30 16:06:22 -0300

    03.11.Organizando melhor nosso codigo.

--- Alura/TypeScript/Parte 1 - Evoluindo seu JavaScript/03.Beneficios da tipagem estática/TypeScript/app/controllers/negociacao-controller.ts
+++ Alura/TypeScript/Parte 1 - Evoluindo seu JavaScript/03.Beneficios da tipagem estática/TypeScript/app/controllers/negociacao-controller.ts
@@ -1,24 +1,28 @@
 import { Negociacao } from "../models/negociacao.js";
 
-export class NegociacaoController{
+export class NegociacaoController {
 
     private inputData: HTMLInputElement;
     private inputQuantidade: HTMLInputElement;
     private inputValor: HTMLInputElement;
 
-    constructor(){
+    constructor() {
 
         this.inputData = document.querySelector('#data');
         this.inputQuantidade = document.querySelector('#quantidade');
         this.inputValor = document.querySelector('#valor');
     }
 
-    adiciona() {
+    adiciona(): void {
+        const negociacao = this.criaNegociacao();
+        console.log(negociacao);
+    }
+
+    criaNegociacao(): Negociacao {
         const exp = /-/g; //Expressão regular. Nesse caso a letra "g" significa "global", na qual serão encontrados todas as ocorrências que surgirem.
         const date = new Date(this.inputData.value.replace(exp, ',')); // O replace vai encontrar todos os "-" ífens do exp e substituí-los por "," vírgulas.
         const quantidade = parseInt(this.inputQuantidade.value);
         const valor = parseFloat(this.inputValor.value);
-        const negociacao = new Negociacao(date, quantidade, valor);
-        console.log(negociacao);
+        return new Negociacao(date, quantidade, valor);
     }
 }
--- Alura/TypeScript/Parte 1 - Evoluindo seu JavaScript/03.Beneficios da tipagem estática/TypeScript/dist/js/controllers/negociacao-controller.js
+++ Alura/TypeScript/Parte 1 - Evoluindo seu JavaScript/03.Beneficios da tipagem estática/TypeScript/dist/js/controllers/negociacao-controller.js
@@ -6,11 +6,14 @@ export class NegociacaoController {
         this.inputValor = document.querySelector('#valor');
     }
     adiciona() {
+        const negociacao = this.criaNegociacao();
+        console.log(negociacao);
+    }
+    criaNegociacao() {
         const exp = /-/g; //Expressão regular. Nesse caso a letra "g" significa "global", na qual serão encontrados todas as ocorrências que surgirem.
         const date = new Date(this.inputData.value.replace(exp, ',')); // O replace vai encontrar todos os "-" ífens do exp e substituí-los por "," vírgulas.
         const quantidade = parseInt(this.inputQuantidade.value);
         const valor = parseFloat(this.inputValor.value);
-        const negociacao = new Negociacao(date, quantidade, valor);
-        console.log(negociacao);
+        return new Negociacao(date, quantidade, valor);
     }
 }
