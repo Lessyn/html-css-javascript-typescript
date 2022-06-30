7ff8db1077a17dc30426466fd3da6614187113f4
Rodrigo Lessyn de Paula <rodrigolessyn@gmail.com>
2022-06-30 08:42:15 -0300

    01.Porque usar TypeScript?

--- Alura/TypeScript/Parte 1 - Evoluindo seu JavaScript/01.Porque usar TypeScript/TypeScript/dist/js/models/negociacao.js
+++ Alura/TypeScript/Parte 1 - Evoluindo seu JavaScript/01.Porque usar TypeScript/TypeScript/dist/js/models/negociacao.js
@@ -1,26 +1,26 @@
-export class Negociacao{
+export class Negociacao {
     #data;
     #quantidade;
     #valor;
 
-    constructor(data, quantidade, valor){
+    constructor(data, quantidade, valor) {
         this.#data = data;
         this.#quantidade = quantidade;
         this.#valor = valor;
     }
-    get data(){
+    get data() {
 
         return this.#data;
     }
-    get quantidade(){
+    get quantidade() {
         return this.#quantidade;
     }
 
-    get valor(){
+    get valor() {
         return this.#valor;
     }
 
-    get volume(){
+    get volume() {
         return this.quantidade * this.#valor;
     }
 }
