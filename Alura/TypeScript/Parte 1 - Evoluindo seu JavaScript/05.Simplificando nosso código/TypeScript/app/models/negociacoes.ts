8524635279808ebb08f1a87ea0d2a1899548dbde
Rodrigo Lessyn de Paula <rodrigolessyn@gmail.com>
2022-07-01 13:49:28 -0300

    05.04.Arrays e Generics.

--- Alura/TypeScript/Parte 1 - Evoluindo seu JavaScript/05.Simplificando nosso código/TypeScript/app/models/negociacoes.ts
+++ Alura/TypeScript/Parte 1 - Evoluindo seu JavaScript/05.Simplificando nosso código/TypeScript/app/models/negociacoes.ts
@@ -1,13 +1,13 @@
 import { Negociacao } from "./negociacao.js";
 
 export class Negociacoes {
-    private negociacoes: Array<Negociacao> = [];
+    private negociacoes: Negociacao[] = [];
 
     adiciona(negociacao: Negociacao) {
         this.negociacoes.push(negociacao);
     }
 
-    lista(): ReadonlyArray<Negociacao> { //Utilizando a função Readonly, o problema da lista é resolvido e essa, não é modificada na classe negociacao-controller.
+    lista(): readonly Negociacao[] { //Utilizando a função Readonly, o problema da lista é resolvido e essa, não é modificada na classe negociacao-controller.
         return this.negociacoes;
     }
 
