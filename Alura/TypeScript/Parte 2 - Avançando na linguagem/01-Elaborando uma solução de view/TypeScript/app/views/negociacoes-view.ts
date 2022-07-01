74e28eab1cb6c9964c65ba7b1e8268e43dde1002
Rodrigo Lessyn de Paula <rodrigolessyn@gmail.com>
2022-07-01 17:27:45 -0300

    01.05.Integrando nossa view a pagina.

--- Alura/TypeScript/Parte 2 - Avançando na linguagem/01-Elaborando uma solução de view/TypeScript/app/views/negociacoes-view.ts
+++ Alura/TypeScript/Parte 2 - Avançando na linguagem/01-Elaborando uma solução de view/TypeScript/app/views/negociacoes-view.ts
@@ -1,17 +1,29 @@
 export class NegociacoesView {
+
+    private elemento: HTMLElement;
+
+    constructor(seletor: string) {
+        this.elemento = document.querySelector(seletor);
+
+    }
+
     template(): string {
         return `
-        <table class= "table table-hover table-bordered"
-             <tread>
+        <table class="table table-hover table-bordered">
+             <thead>
                    <tr>
                       <th>DATA</th>
                       <th>QUANTIDADE</th>
                       <th>VALOR</th>        
                   </tr>
-             </tread>
+             </thead>
              <Tbody>
              </Tbody>
         
         </table>`;
+    }
+
+    update(): void {
+        this.elemento.innerHTML = this.template();
     }
 }
--- Alura/TypeScript/Parte 2 - Avançando na linguagem/01-Elaborando uma solução de view/TypeScript/dist/js/views/negociacoes-view.js
+++ Alura/TypeScript/Parte 2 - Avançando na linguagem/01-Elaborando uma solução de view/TypeScript/dist/js/views/negociacoes-view.js
@@ -1,17 +1,23 @@
 export class NegociacoesView {
+    constructor(seletor) {
+        this.elemento = document.querySelector(seletor);
+    }
     template() {
         return `
-        <table class= "table table-hover table-bordered"
-             <tread>
+        <table class="table table-hover table-bordered">
+             <thead>
                    <tr>
                       <th>DATA</th>
                       <th>QUANTIDADE</th>
                       <th>VALOR</th>        
                   </tr>
-             </tread>
+             </thead>
              <Tbody>
              </Tbody>
         
         </table>`;
+    }
+    update() {
+        this.elemento.innerHTML = this.template();
     }
 }
