0e94fb267b7e316a3cae31a4ae3ca9bdca5e7494
Rodrigo Lessyn de Paula <rodrigolessyn@gmail.com>
2022-07-05 17:02:19 -0300

    Importando os arquivos do modulo 02.

---
+++ Alura/TypeScript/Parte 2 - Avançando na linguagem/03-Visibilidade de métodos e Enumeration/TypeScript/app/models/negociacoes.ts
@@ -0,0 +1,15 @@
+import { Negociacao } from "./negociacao.js";
+
+export class Negociacoes {
+
+    private negociacoes: Negociacao[] = [];
+
+    adiciona(negociacao: Negociacao) {
+
+        this.negociacoes.push(negociacao);
+    }
+
+    lista(): readonly Negociacao[] { //Utilizando a função Readonly, o problema da lista é resolvido e essa, não é modificada na classe negociacao-controller.
+        return this.negociacoes;
+    }
+}
---
+++ Alura/TypeScript/Parte 2 - Avançando na linguagem/03-Visibilidade de métodos e Enumeration/TypeScript/dist/js/controllers/negociacao-controller.js
@@ -0,0 +1,35 @@
+import { Negociacao } from "../models/negociacao.js";
+import { Negociacoes } from "../models/negociacoes.js";
+import { MensagemView } from "../views/mensagem-view.js";
+import { NegociacoesView } from "../views/negociacoes-view.js";
+export class NegociacaoController {
+    constructor() {
+        this.negociacoes = new Negociacoes();
+        this.negociacoesView = new NegociacoesView('#negociacoesView');
+        this.mensagemView = new MensagemView('#mensagemView');
+        this.inputData = document.querySelector('#data');
+        this.inputQuantidade = document.querySelector('#quantidade');
+        this.inputValor = document.querySelector('#valor');
+        this.negociacoesView.update(this.negociacoes);
+    }
+    adiciona() {
+        const negociacao = this.criaNegociacao();
+        this.negociacoes.adiciona(negociacao);
+        this.negociacoesView.update(this.negociacoes);
+        this.mensagemView.update("Negociação adicionada com sucesso!");
+        this.limparFormulario();
+    }
+    criaNegociacao() {
+        const exp = /-/g; //Expressão regular. Nesse caso a letra "g" significa "global", na qual serão encontrados todas as ocorrências que surgirem.
+        const date = new Date(this.inputData.value.replace(exp, ',')); // O replace vai encontrar todos os "-" ífens do exp e substituí-los por "," vírgulas.
+        const quantidade = parseInt(this.inputQuantidade.value);
+        const valor = parseFloat(this.inputValor.value);
+        return new Negociacao(date, quantidade, valor);
+    }
+    limparFormulario() {
+        this.inputData.value = '';
+        this.inputQuantidade.value = '1';
+        this.inputValor.value = '0.0';
+        this.inputData.focus();
+    }
+}
