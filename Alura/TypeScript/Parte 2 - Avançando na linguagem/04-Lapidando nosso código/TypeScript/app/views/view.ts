20c222fa4e340612b8de732d05d076402fa2c4b0
Rodrigo Lessyn de Paula <rodrigolessyn@gmail.com>
2022-07-06 15:10:21 -0300

    04.05.Protegendo nosso template.

--- Alura/TypeScript/Parte 2 - Avançando na linguagem/04-Lapidando nosso código/TypeScript/app/views/view.ts
+++ Alura/TypeScript/Parte 2 - Avançando na linguagem/04-Lapidando nosso código/TypeScript/app/views/view.ts
@@ -2,7 +2,9 @@ export abstract class View<T> {
 
     protected elemento: HTMLElement;
 
-    constructor(seletor: string) {
+    private escapar = false;
+
+    constructor(seletor: string, escapar: boolean) {
 
         this.elemento = document.querySelector(seletor);
     }
@@ -11,7 +13,10 @@ export abstract class View<T> {
     protected abstract template(model: T): string;
 
     public update(model: T): void {
-        const template = this.template(model);
+        let template = this.template(model);
+        if (this.escapar) {
+            template = template.replace(/<script>[\s\S]*?<\/script>/, '');
+        }       
         this.elemento.innerHTML = template;
     }
 }
