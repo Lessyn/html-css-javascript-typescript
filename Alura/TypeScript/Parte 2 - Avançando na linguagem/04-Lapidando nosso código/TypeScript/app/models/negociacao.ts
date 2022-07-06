7bd5892cb318313ea49eceeeb6d515c0bc516c9b
Rodrigo Lessyn de Paula <rodrigolessyn@gmail.com>
2022-07-06 14:20:19 -0300

    04.03.Metodos estaticos.

--- Alura/TypeScript/Parte 2 - Avançando na linguagem/04-Lapidando nosso código/TypeScript/app/models/negociacao.ts
+++ Alura/TypeScript/Parte 2 - Avançando na linguagem/04-Lapidando nosso código/TypeScript/app/models/negociacao.ts
@@ -17,7 +17,7 @@ export class Negociacao {
         return (this.quantidade * this.valor);
     }
 
-    public criaDe(dataString: string, quantidadeString: string, valorString: string): Negociacao {
+    public static criaDe(dataString: string, quantidadeString: string, valorString: string): Negociacao {
         const exp = /-/g; //Expressão regular. Nesse caso a letra "g" significa "global", na qual serão encontrados todas as ocorrências que surgirem.
         const date = new Date(dataString.replace(exp, ',')); // O replace vai encontrar todos os "-" ífens do exp e substituí-los por "," vírgulas.
         const quantidade = parseInt(quantidadeString);
--- Alura/TypeScript/Parte 2 - Avançando na linguagem/04-Lapidando nosso código/TypeScript/dist/js/models/negociacao.js
+++ Alura/TypeScript/Parte 2 - Avançando na linguagem/04-Lapidando nosso código/TypeScript/dist/js/models/negociacao.js
@@ -11,7 +11,7 @@ export class Negociacao {
     get volume() {
         return (this.quantidade * this.valor);
     }
-    criaDe(dataString, quantidadeString, valorString) {
+    static criaDe(dataString, quantidadeString, valorString) {
         const exp = /-/g; //Expressão regular. Nesse caso a letra "g" significa "global", na qual serão encontrados todas as ocorrências que surgirem.
         const date = new Date(dataString.replace(exp, ',')); // O replace vai encontrar todos os "-" ífens do exp e substituí-los por "," vírgulas.
         const quantidade = parseInt(quantidadeString);
