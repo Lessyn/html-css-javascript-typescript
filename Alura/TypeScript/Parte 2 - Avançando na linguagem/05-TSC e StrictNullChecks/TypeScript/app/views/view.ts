aba8a265780947b17b776a6a394365e993233144
Rodrigo Lessyn de Paula <rodrigolessyn@gmail.com>
2022-07-07 08:59:09 -0300

    05.08.Ultimos ajustes.

--- Alura/TypeScript/Parte 2 - Avançando na linguagem/05-TSC e StrictNullChecks/TypeScript/app/views/view.ts
+++ Alura/TypeScript/Parte 2 - Avançando na linguagem/05-TSC e StrictNullChecks/TypeScript/app/views/view.ts
@@ -6,9 +6,15 @@ export abstract class View<T> {
 
     constructor(seletor: string, escapar?: boolean) {
 
-        this.elemento = document.querySelector(seletor);
+        const elemento = document.querySelector(seletor);
+        if (elemento) {
+            this.elemento = elemento as HTMLElement;
+        }
+        else {
+            throw Error(`Seletor ${seletor} não existe no DOM. Verifique.`);
+        }
 
-        if(escapar) {
+        if (escapar) {
             this.escapar = escapar;
         }
     }
@@ -20,7 +26,7 @@ export abstract class View<T> {
         let template = this.template(model);
         if (this.escapar) {
             template = template.replace(/<script>[\s\S]*?<\/script>/, '');
-        }       
+        }
         this.elemento.innerHTML = template;
     }
 }
