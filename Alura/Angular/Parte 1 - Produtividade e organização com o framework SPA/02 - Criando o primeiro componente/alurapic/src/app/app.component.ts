6f04e8572fd04872fd07cb2ac9fd713de78d4bd2
Rodrigo Lessyn de Paula <rodrigolessyn@gmail.com>
2022-07-08 14:08:58 -0300

    Importando os arquivos do modulo 01.

---
+++ Alura/Angular/Parte 1 - Produtividade e organização com o framework SPA/02 - Criando o primeiro componente/alurapic/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'alurapic';
+  description = 'Leão';
+  url = "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/White_Lion.jpg/330px-White_Lion.jpg";
+}
