2402143e85c40366e241f60e290f6ebdcad0c617
Rodrigo Lessyn de Paula <rodrigolessyn@gmail.com>
2022-07-08 15:05:12 -0300

    02.02.Criando o primeiro componente.

--- Alura/Angular/Parte 1 - Produtividade e organização com o framework SPA/02 - Criando o primeiro componente/alurapic/src/app/app.component.ts
+++ Alura/Angular/Parte 1 - Produtividade e organização com o framework SPA/02 - Criando o primeiro componente/alurapic/src/app/app.component.ts
@@ -6,7 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'alurapic';
-  description = 'Leão';
-  url = "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/White_Lion.jpg/330px-White_Lion.jpg";
+  
+  
 }
---
+++ Alura/Angular/Parte 1 - Produtividade e organização com o framework SPA/02 - Criando o primeiro componente/alurapic/src/app/photo/photo.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'ap-photo',
+    templateUrl: 'photo.component.html'
+})
+export class PhotoComponent {
+    
+    description = 'Leão';
+    url = "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/White_Lion.jpg/330px-White_Lion.jpg";
+}
