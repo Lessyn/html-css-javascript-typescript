5593a8fecd5eb57b1917aecbf0453db601db43ff
Rodrigo Lessyn de Paula <rodrigolessyn@gmail.com>
2022-07-12 10:49:45 -0300

    02.09.Utilizando a primeira diretiva.

--- Alura/Angular/Parte 1 - Produtividade e organização com o framework SPA/02 - Criando o primeiro componente/alurapic/src/app/app.component.ts
+++ Alura/Angular/Parte 1 - Produtividade e organização com o framework SPA/02 - Criando o primeiro componente/alurapic/src/app/app.component.ts
@@ -6,6 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  
-  
+
+  photos = [
+    {
+      url: "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/White_Lion.jpg/330px-White_Lion.jpg",
+      description: "Leao"
+    },
+    {
+      url: "https://img.r7.com/images/moya-leao-branco-albino-atracao-glen-garriff-conservation-lion-sanctuary-23032021161517683?dimensions=771x420&no_crop=true",
+      description: "Leoa"
+    }
+  ]
+
 }
