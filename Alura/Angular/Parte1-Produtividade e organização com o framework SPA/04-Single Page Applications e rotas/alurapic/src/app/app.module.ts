ee98840d0b6f6b21e684aeaca9ea5e6f3c5093ce
Rodrigo Lessyn de Paula <rodrigolessyn@gmail.com>
2022-07-14 17:24:52 -0300

    04.08.Lidando com rotas inexistentes.

--- Alura/Angular/Parte1-Produtividade e organização com o framework SPA/04-Single Page Applications e rotas/alurapic/src/app/app.module.ts
+++ Alura/Angular/Parte1-Produtividade e organização com o framework SPA/04-Single Page Applications e rotas/alurapic/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app.routing.module';
+import { ErrorsModule } from './errors/errors.module';
 import { PhotosModule } from './photos/photos.module';
 
 @NgModule({
@@ -11,7 +12,8 @@ import { PhotosModule } from './photos/photos.module';
   imports: [
     BrowserModule,
     PhotosModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ErrorsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
