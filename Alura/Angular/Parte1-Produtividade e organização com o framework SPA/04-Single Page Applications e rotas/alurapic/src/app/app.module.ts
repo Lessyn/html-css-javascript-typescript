0e300815c9917ca8be880f1bcca44fe7365248a5
Rodrigo Lessyn de Paula <rodrigolessyn@gmail.com>
2022-07-14 17:08:30 -0300

    04.15.Single Page Application e Rotas.

--- Alura/Angular/Parte1-Produtividade e organização com o framework SPA/04-Single Page Applications e rotas/alurapic/src/app/app.module.ts
+++ Alura/Angular/Parte1-Produtividade e organização com o framework SPA/04-Single Page Applications e rotas/alurapic/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app.routing.module';
 import { PhotosModule } from './photos/photos.module';
 
 @NgModule({
@@ -9,7 +10,8 @@ import { PhotosModule } from './photos/photos.module';
   ],
   imports: [
     BrowserModule,
-    PhotosModule
+    PhotosModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
