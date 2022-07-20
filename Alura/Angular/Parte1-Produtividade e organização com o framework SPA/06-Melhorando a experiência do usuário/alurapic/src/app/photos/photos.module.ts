b4f32a1338b0fad9d9af49214ea877900b4b9a1d
Rodrigo Lessyn de Paula <rodrigolessyn@gmail.com>
2022-07-20 17:06:22 -0300

    06.10.LoadButton.

--- Alura/Angular/Parte1-Produtividade e organização com o framework SPA/06-Melhorando a experiência do usuário/alurapic/src/app/photos/photos.module.ts
+++ Alura/Angular/Parte1-Produtividade e organização com o framework SPA/06-Melhorando a experiência do usuário/alurapic/src/app/photos/photos.module.ts
@@ -8,6 +8,7 @@ import { PhotoFormComponent } from './photo-form/photo-form.component';
 import { CapitulosComponent } from './capitulos/capitulos.component';
 import { PhotosComponent } from './photo-list/photos/photos.component';
 import { FilterByDescritpion } from './photo-list/filter-by-description.pipe';
+import { LoadButtonComponent } from './photo-list/load-button/load-button.component';
 
 
 @NgModule({
@@ -16,7 +17,8 @@ declarations: [PhotoComponent,
                PhotoFormComponent,
                CapitulosComponent,
                PhotosComponent,
-               FilterByDescritpion
+               FilterByDescritpion,
+               LoadButtonComponent
             ],
 imports : [HttpClientModule,
            CommonModule
