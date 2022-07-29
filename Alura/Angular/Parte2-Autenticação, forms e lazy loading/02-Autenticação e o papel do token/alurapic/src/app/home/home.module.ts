79796a96787a035fc961c5b6e568915be8121e7d
Rodrigo Lessyn de Paula <rodrigolessyn@gmail.com>
2022-07-29 09:40:38 -0300

    Importando os arquivos da Parte1.

---
+++ Alura/Angular/Parte2-Autenticação, forms e lazy loading/02-Autenticação e o papel do token/alurapic/src/app/home/home.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { SignInComponent } from './signin/signin.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { VMessageModule } from '../shared/component/vmessage/vmessage.module';
+import { RouterModule } from '@angular/router';
+
+@NgModule({
+    declarations: [
+        SignInComponent
+    ],
+    imports: [
+        CommonModule,
+        ReactiveFormsModule,
+        VMessageModule,
+        RouterModule
+    ]
+})
+
+export class HomeModule{ }
