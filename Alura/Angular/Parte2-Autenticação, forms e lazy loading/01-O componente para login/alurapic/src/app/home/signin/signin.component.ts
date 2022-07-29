c513d43a8d8a684855559b03a6a110494d45b111
Rodrigo Lessyn de Paula <rodrigolessyn@gmail.com>
2022-07-29 09:27:24 -0300

    01.12.Detectando a plataforma de execucao.

--- Alura/Angular/Parte2-Autenticação, forms e lazy loading/01-O componente para login/alurapic/src/app/home/signin/signin.component.ts
+++ Alura/Angular/Parte2-Autenticação, forms e lazy loading/01-O componente para login/alurapic/src/app/home/signin/signin.component.ts
@@ -1,7 +1,8 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-import { AuthService } from 'src/app/core/auth.service';
+import { AuthService } from 'src/app/core/auth/auth.service';
+import { PlatformDetectorService } from 'src/app/core/plataform-detector/platform-detector.service';
 
 @Component({
     templateUrl: './signin.component.html'
@@ -12,12 +13,13 @@ export class SignInComponent implements OnInit{
     loginForm: FormGroup;
     @ViewChild('userNameInput') userNameInput: ElementRef<HTMLInputElement>;
 
-    constructor(private formBuilder: FormBuilder,
-                private authService: AuthService,
-                private router: Router) { }
+    constructor(private _formBuilder: FormBuilder,
+                private _authService: AuthService,
+                private _router: Router,
+                private _platformDetectorService: PlatformDetectorService) { }
 
     ngOnInit(): void {
-        this.loginForm = this.formBuilder.group({
+        this.loginForm = this._formBuilder.group({
             userName: ['', Validators.required],
             password: ['', Validators.required]
         });
@@ -28,13 +30,14 @@ export class SignInComponent implements OnInit{
         const userName = this.loginForm.get('userName')?.value;
         const password = this.loginForm.get('password')?.value;
 
-        this.authService
+        this._authService
         .authenticate(userName, password)
         .subscribe(
-             () => this.router.navigate(['user', userName]),
+             () => this._router.navigate(['user', userName]),
             err => {
                 console.log(err);
                 this.loginForm.reset();
+                this._platformDetectorService.isPlatformBrowser() && //detecta em qual plataforma a aplicação está sendo executada que nesse caso aqui, é um browser. E quando é um browser, ele aplica um focus no login.
                 this.userNameInput.nativeElement.focus();
                 alert('Invalid user name or password!');
             }
