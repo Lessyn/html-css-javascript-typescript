a2adec114c2e0d2dac3e1d5d865d164255134bb9
Rodrigo Lessyn de Paula <rodrigolessyn@gmail.com>
2022-08-09 15:06:53 -0300

    01.04.O formulario upload de fotos.

--- Alura/Angular/Parte3-Upload, build e novos componentes/01-Upload de fotos/alurapic/src/app/photos/photo-form/photo-form.module.ts
+++ Alura/Angular/Parte3-Upload, build e novos componentes/01-Upload de fotos/alurapic/src/app/photos/photo-form/photo-form.module.ts
@@ -1,12 +1,21 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { VMessageModule } from 'src/app/shared/component/vmessage/vmessage.module';
 import { PhotoFormComponent } from './photo-form.component';
 
 @NgModule ({
-    declarations: [
+    declarations: 
+    [
         PhotoFormComponent
     ],
 
-    imports: [CommonModule]
+    imports: 
+    [
+        CommonModule,
+        ReactiveFormsModule,
+        VMessageModule,
+        FormsModule
+    ]
 })
 export class PhotoFormModule{}
