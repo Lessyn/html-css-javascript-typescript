6d2b420141c95fbc2ccda5b9ace9200af18d8390
Rodrigo Lessyn de Paula <rodrigolessyn@gmail.com>
2022-08-10 15:52:04 -0300

    Importando os modulos do capitulo 02.

---
+++ Alura/Angular/Parte3-Upload, build e novos componentes/03-Detalhes da foto/alurapic/src/app/app.routing.module.ts
@@ -0,0 +1,57 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { AuthGuard } from './core/auth/auth.guard';
+
+import { NotFoundComponent } from './errors/not-found/not-found.component';
+import { CapitulosComponent } from './photos/capitulos/capitulos.component';
+import { PhotoFormComponent } from './photos/photo-form/photo-form.component';
+import { PhotoListComponent } from './photos/photo-list/photo-list.component';
+import { PhotoListResolver } from './photos/photo-list/photo-list.resolver';
+
+const routes: Routes = [
+
+    {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'home'
+    },
+
+    {
+        path: 'home',       
+        loadChildren: () => import('./home/home.module').then(h => h.HomeModule)
+       
+    },
+   
+    {
+        path: 'user/:userName',
+        component: PhotoListComponent,
+        resolve: {
+            photos: PhotoListResolver
+        }
+    
+    },
+
+    {
+        path: 'capitulos',
+        component: CapitulosComponent
+    },
+
+    {
+        path: 'p/add',
+        component: PhotoFormComponent,
+        canActivate:[AuthGuard]
+    },
+
+    {
+        path: '**',
+        component: NotFoundComponent
+    }
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes, { useHash: true})],
+    exports: [RouterModule]
+})
+
+export class AppRoutingModule { }
---
+++ Alura/Angular/Parte3-Upload, build e novos componentes/03-Detalhes da foto/alurapic/src/app/photos/photo-form/photo-form.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { PhotoService } from '../photo/photo.service';
+
+@Component({
+  selector: 'ap-photo-form',
+  templateUrl: './photo-form.component.html',
+  styleUrls: ['./photo-form.component.css']
+})
+export class PhotoFormComponent implements OnInit {
+
+  photoForm: FormGroup;
+  file: File;
+  preview: string;
+
+  constructor(
+    private _formBuilder: FormBuilder,
+    private _photoService: PhotoService,
+    private _router:Router
+    ) { }
+
+  ngOnInit(): void {
+
+    this.photoForm = this._formBuilder.group({
+      file: ['', Validators.required],
+      description:['', Validators.maxLength(300)],
+      allowComments:[true]
+    })
+  }
+
+  upload(){
+    const description = this.photoForm.get('description').value;
+    const allowComments = this.photoForm.get('allowComments').value;
+    this._photoService
+    .upload(description, allowComments, this.file)
+    .subscribe(() => this._router.navigate(['']));
+    
+  }
+
+  handleFile(file: File){
+    this.file = file;
+    const reader = new FileReader();
+    reader.onload = (event: any) => this.preview = event.target.result;
+    reader.readAsDataURL(file);
+  }
+
+}
