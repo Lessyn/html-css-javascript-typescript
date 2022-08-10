0bfdad42ec04a8df6bfc0589361564dd9771924b
Rodrigo Lessyn de Paula <rodrigolessyn@gmail.com>
2022-08-10 16:59:01 -0300

    03.02.Rotas parametrizadas.

---
+++ Alura/Angular/Parte3-Upload, build e novos componentes/03-Detalhes da foto/alurapic/src/app/photos/photo-details/photo-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ap-photo-details',
+  templateUrl: './photo-details.component.html',
+  styleUrls: ['./photo-details.component.css']
+})
+export class PhotoDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
