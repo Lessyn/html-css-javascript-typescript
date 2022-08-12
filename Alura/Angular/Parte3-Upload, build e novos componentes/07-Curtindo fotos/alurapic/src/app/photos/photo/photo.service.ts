5882afd5c40afd9c494416fe5d78d2ff798b46cb
Rodrigo Lessyn de Paula <rodrigolessyn@gmail.com>
2022-08-12 16:51:40 -0300

    07.04.Preparando o servico.

--- Alura/Angular/Parte3-Upload, build e novos componentes/07-Curtindo fotos/alurapic/src/app/photos/photo/photo.service.ts
+++ Alura/Angular/Parte3-Upload, build e novos componentes/07-Curtindo fotos/alurapic/src/app/photos/photo/photo.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient, HttpParams } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { catchError, map, of, throwError } from 'rxjs';
 import { Photo } from "./photo";
 import { PhotoComment} from "./photo-comment"
 
@@ -52,5 +53,14 @@ export class PhotoService {
 
     removePhoto(photoId: number){
         return this._http.delete(API + '/photos/' + photoId);
+    }
+
+    like(photoId: number){
+        return this._http
+        .post(API + '/photos/' + photoId + '/like', {observe: 'response'})
+        .pipe(map(res => true))
+        .pipe(catchError(err => {
+            return err.status == '304' ? of(false) : throwError(err);
+        }))
     }
 }
