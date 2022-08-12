6f4330de3468d43b8cc8fa2b785478b89ad590cc
Rodrigo Lessyn de Paula <rodrigolessyn@gmail.com>
2022-08-12 10:47:00 -0300

    06.04.A camada de servico do componente.

--- Alura/Angular/Parte3-Upload, build e novos componentes/06-Componente de notificação/alurapic/src/app/shared/component/alert/alert.service.ts
+++ Alura/Angular/Parte3-Upload, build e novos componentes/06-Componente de notificação/alurapic/src/app/shared/component/alert/alert.service.ts
@@ -1,1 +1,37 @@
-export class AlertService{}
+import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+import { Alert, AlertType } from './alert';
+
+@Injectable({
+    providedIn: 'root'
+})
+
+export class AlertService{
+
+    alertSubject: Subject<Alert>;
+
+    sucess(message: string){
+        this._alert(AlertType.SUCESS, message); 
+    }
+
+    warning(message: string){
+        this._alert(AlertType.WARNING, message);
+    }
+
+    danger(message: string){
+        this._alert(AlertType.DANGER, message);
+    }
+
+    info(message: string){
+        this._alert(AlertType.INFO, message);
+    }
+
+    private _alert(alertType: AlertType, message: string){
+
+        this.alertSubject.next(new Alert(alertType, message));
+    }
+
+    getAlert(){
+        return this.alertSubject.asObservable(); 
+    }
+}
