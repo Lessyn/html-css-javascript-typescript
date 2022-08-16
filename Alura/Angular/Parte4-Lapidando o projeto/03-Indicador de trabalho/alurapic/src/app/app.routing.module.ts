287b7556c1ec0c8704f837dd8fc014af600d7ad7
Rodrigo Lessyn de Paula <rodrigolessyn@gmail.com>
2022-08-16 11:04:57 -0300

    Importando os modulos do capitulo 02.

---
+++ Alura/Angular/Parte4-Lapidando o projeto/03-Indicador de trabalho/alurapic/src/app/app.routing.module.ts
@@ -0,0 +1,85 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { AuthGuard } from './core/auth/auth.guard';
+
+import { NotFoundComponent } from './errors/not-found/not-found.component';
+import { CapitulosComponent } from './photos/capitulos/capitulos.component';
+import { PhotoDetailsComponent } from './photos/photo-details/photo-details.component';
+import { PhotoFormComponent } from './photos/photo-form/photo-form.component';
+import { PhotoListComponent } from './photos/photo-list/photo-list.component';
+import { PhotoListResolver } from './photos/photo-list/photo-list.resolver';
+
+const routes: Routes = [
+
+    {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'home'
+    },
+
+    {
+        path: 'home',       
+        loadChildren: () => import('./home/home.module').then(h => h.HomeModule)
+       
+    },
+   
+    {
+        path: 'user/:userName',
+        pathMatch: 'full',
+        component: PhotoListComponent,
+        resolve: {
+            photos: PhotoListResolver
+        },
+        data: {
+            title:'Timeline'
+        }
+    
+    },
+
+    {
+        path: 'capitulos',
+        component: CapitulosComponent,
+        data:{
+            title: 'Capitulos'
+        }
+    },
+
+    {
+        path: 'p/add',
+        component: PhotoFormComponent,
+        canActivate:[AuthGuard],
+        data:{
+            title: 'Photo upload'
+        }
+    },
+
+    {
+        path: 'p/:photoId',
+        component: PhotoDetailsComponent,
+        data: {
+            title: 'Photo detail'
+        }
+        
+    },
+
+    {
+        path: 'not-found',
+        component: NotFoundComponent,
+        data: {
+            title: 'not-found'
+        }
+    },
+
+    {
+        path: '**',
+        redirectTo: 'Not-Found'
+    }
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes, { useHash: true})],
+    exports: [RouterModule]
+})
+
+export class AppRoutingModule { }
