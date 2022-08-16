aeade4f53a1de13e810bfe8304ef6e2752666c67
Rodrigo Lessyn de Paula <rodrigolessyn@gmail.com>
2022-08-16 10:59:43 -0300

    Renomeando e organizando as pastas.

--- Alura/Angular/Parte4-Lapidando o projeto/Indicador de trabalho/alurapic/src/app/shared/component/vmessage/vmessage.componenrt.ts
+++
@@ -1,9 +0,0 @@
-import { Component, Input } from '@angular/core';
-
-@Component({
-    selector: 'ap-vmessage',
-    templateUrl: './vmessage.Component.html'
-})
-export class VMessageComponent {
-    @Input() text = '';
- }
