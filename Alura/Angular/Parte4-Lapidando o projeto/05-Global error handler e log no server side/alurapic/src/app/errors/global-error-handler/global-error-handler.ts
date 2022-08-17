b5e76bc8fbedef682d9f4ca653bfd752af502ae6
Rodrigo Lessyn de Paula <rodrigolessyn@gmail.com>
2022-08-17 09:23:00 -0300

    05.06.Exibindo mais informacoes.

--- Alura/Angular/Parte4-Lapidando o projeto/05-Global error handler e log no server side/alurapic/src/app/errors/global-error-handler/global-error-handler.ts
+++ Alura/Angular/Parte4-Lapidando o projeto/05-Global error handler e log no server side/alurapic/src/app/errors/global-error-handler/global-error-handler.ts
@@ -1,16 +1,38 @@
-import { ErrorHandler } from '@angular/core';
+import { LocationStrategy, PathLocationStrategy } from '@angular/common';
+import { ErrorHandler, Injectable, Injector } from '@angular/core';
 import * as StackTrace from 'stacktrace-js';
 
+import { UserService } from 'src/app/core/user/user.service';
+
+@Injectable()
+
 export class GlobalErrorHandler implements ErrorHandler{
+    constructor(private _injector: Injector){}
+
     handleError(error: any): void {
         console.log('Passei pelo handler');
+
+        const location = this._injector.get(LocationStrategy);
+        const userService = this._injector.get(UserService);
+
+        const url = location instanceof PathLocationStrategy
+        ? location.path()
+        : '';
+
+        const message = error.message 
+        ? error.message 
+        : error.toString();
+
         StackTrace
         .fromError(error)
         .then(stackFrames => {
             const stackAsString = stackFrames
             .map(sf => sf.toString())
             .join('\n');
+            console.log(message);
             console.log(stackAsString);
+            console.log("O que será enviado para o servidor?");            
+            console.log({message, url, userName: userService.getUserName(), stack: stackAsString});
         })
     }
 }
