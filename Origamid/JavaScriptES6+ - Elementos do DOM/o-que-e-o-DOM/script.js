bc3cdb16102c07c22fecc6d24d5cd96396f9ee4f
Rodrigo Lessyn de Paula <rodrigolessyn@gmail.com>
2025-02-11 10:19:48 -0300

    Origamid - Elementos do DOM

---
+++ Origamid/JavaScriptES6+ - Elementos do DOM/o-que-e-o-DOM/script.js
@@ -0,0 +1,11 @@
+// const hrefpagina = window.location.href;
+// console.log(hrefpagina);
+
+const elementoAtivo = document.querySelectorAll('.ativo')
+ console.log(elementoAtivo);
+
+ const linguagem = window.navigator.language;
+ console.log(linguagem);
+
+ const windowWidth = window.innerWidth;
+ console.log(windowWidth);
