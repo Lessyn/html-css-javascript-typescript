987f76de658501cd485e8a8cb4aba8b5d5897f6b
Rodrigo Lessyn de Paula <rodrigolessyn@gmail.com>
2025-02-11 14:51:29 -0300

    Origamid - Boolean e Condicionais

---
+++ Origamid/JavaScriptES6+/boolean-e-condicionais/script.js
@@ -0,0 +1,28 @@
+var idadeRodrigo = 39;
+var idadeDaiana = 40;
+
+if (idadeRodrigo > idadeDaiana){
+console.log('É maior')
+}
+else if (idadeRodrigo < idadeDaiana){
+    console.log('É menor')
+}
+else{
+    console.log('É igual')
+}
+ 
+
+var expressao = (5 - 2) && (5 - '') && (5 - 2);
+console.log(expressao);
+
+var nome = 'Rodrigo';
+
+var idade = 45;
+
+var posuiDoutorado = false;
+
+var empregoFuturo;
+
+var dinheiroNaConta = 0;
+
+console.log(!!nome, !!idade, !!posuiDoutorado, !!empregoFuturo, !!dinheiroNaConta);
