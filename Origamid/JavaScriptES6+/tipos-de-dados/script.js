e0b5e126e9e7f60b178f19b3d65ee9f588c72324
Rodrigo Lessyn de Paula <rodrigolessyn@gmail.com>
2025-02-11 10:21:38 -0300

    Origamid - Tipos de Dados

---
+++ Origamid/JavaScriptES6+/tipos-de-dados/script.js
@@ -0,0 +1,27 @@
+// var idade = 45;
+// var simbolo = Symbol();
+// var nome = "Rodrigo";
+// console.log(typeof nome);
+
+// var nome = 'Rodrigo'
+// var sobrenome = 'Lessyn'
+// var nomecompleto = nome+' '+sobrenome
+
+// console.log(nomecompleto)
+
+var nome = 'Rodrigo';
+console.log(typeof nome);
+
+var peso = '88';
+console.log(typeof peso);
+
+var idade = 45;
+
+var sobrenome = 'Lessyn';
+
+var nomecompleto = `${nome} ${sobrenome}`;
+
+console.log(nomecompleto)
+
+
+
