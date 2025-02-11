fd5a8ad3d0fcd0550571ec21cc4dad127c155f62
Rodrigo Lessyn de Paula <rodrigolessyn@gmail.com>
2025-02-11 10:22:17 -0300

    Origamid - Operador Ternario

---
+++ Origamid/JavaScriptES6+/operador-ternario/script.js
@@ -0,0 +1,13 @@
+var scroll = 500;
+scroll +=1000;
+console.log(scroll);
+
+var possuiCarro = true;
+var possuiCasa = false;
+
+var darCredito;
+darCredito = (possuiCarro && possuiCasa )
+console.log(darCredito)
+
+darCredito = (possuiCarro && possuiCasa) ? 'Dar crédito ao cliente': 'Negar crédito';
+console.log(darCredito);
