9af74c8b69a246a5fe04155bbe2f3b13e69fa92d
Rodrigo Lessyn de Paula <rodrigolessyn@gmail.com>
2025-02-11 10:20:16 -0300

    Origamid - Variaveis

---
+++ Origamid/JavaScriptES6+/variaveis/script.js
@@ -0,0 +1,11 @@
+var nome = 'Rodrigo';
+
+var idade = 45;
+
+var comida;
+
+comida = 'lazanha';
+
+var peso = 85, sexo = 'Masculino', genero = 'Cisgenero', altura = 1.76, filhos = 4;
+
+console.log("Nome: "+nome,"Idade: "+idade, "comida: "+comida, "peso: "+peso, "sexo: "+sexo, "altura: "+altura, "filhos: "+filhos);
